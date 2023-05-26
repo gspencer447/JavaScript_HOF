cd8c0dbf9475393146dc26f7a361ac540c3b7f3a
Graham Spencer <gspencer447@yahoo.com>
2023-05-26 15:34:34 -0500

    completed exercises 1-3 after unsuccessfully creating as a repo the first time

--- app.js
+++ app.js
@@ -1,4 +1,77 @@
-console.log("Hello World!\n==========\n");
-
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function plus (number){
+    return (plusNumber) => {
+        return number + plusNumber ;
+    }
+}
+let plus15 = plus(15);
+console.log(plus15(10));
+
+// Exercise 2 Section
+// let users = [
+//     {
+//     name: "Frodo",
+//     age: 50,
+//     score: 85,
+//     isActive: false,
+//     },
+//     {
+//     name: "Sam",
+//     age: 38,
+//     score: 94,
+//     isActive: true,
+//     },
+//     {
+//     name: "Merry",
+//     age: 36,
+//     score: 82,
+//     isActive: true,
+//     },
+//     {
+//     name: "Pippin",
+//     age: 26,
+//     score: 77,
+//     isActive: false,
+//     },
+// ];
+
+// users.forEach(function(user){
+//     console.log(user.name);
+// });
+
+// Exercise 3
+
+let users = [
+    {
+    name: "Frodo",
+    age: 50,
+    score: 85,
+    isActive: false,
+    },
+    {
+    name: "Sam",
+    age: 38,
+    score: 94,
+    isActive: true,
+    },
+    {
+    name: "Merry",
+    age: 36,
+    score: 82,
+    isActive: true,
+    },
+    {
+    name: "Pippin",
+    age: 26,
+    score: 77,
+    isActive: false,
+    },
+];
+
+let namesAndScores = users.map((user) => {
+    return {name: user.name, score: user.score };
+});
+
+console.log(namesAndScores);
